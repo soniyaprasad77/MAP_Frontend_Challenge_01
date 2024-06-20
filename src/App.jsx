d27f631a62c40fca89aae69d61e575c641d1d548
Your Name <soniyagupta82270@gmail.com>
2024-06-20 12:15:25 +0530

    Setup Tailwind CSS for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1>Initialized App</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
